e6f0c3b05d9e7b47572ff3283704fa6126eb9e73
K.L. Λ <k.l.lambda@gmail.com>
2026-01-30 18:34:51 +0800

    fix: remove lilypondDecoder from main export (requires optional lotus)

--- source/lilylet/index.ts
+++ source/lilylet/index.ts
@@ -5,11 +5,8 @@ export * from "./serializer";
 
 import * as meiEncoder from "./meiEncoder";
 import * as musicXmlDecoder from "./musicXmlDecoder";
-import * as lilypondDecoder from "./lilypondDecoder";
-
 
 export {
 	meiEncoder,
 	musicXmlDecoder,
-	lilypondDecoder,
 };
