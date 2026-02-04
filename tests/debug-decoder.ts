ac15bac65d4524aba811b7377f56a83f218c06a8
K.L. Λ <k.l.lambda@gmail.com>
2026-02-04 17:45:02 +0800

    chore: remove debug file

--- tests/debug-decoder.ts
+++
@@ -1,6 +0,0 @@
-import { decode } from "../source/lilylet/lilypondDecoder.js";
-import * as fs from "fs";
-
-const ly = fs.readFileSync("tests/output/lilypond-roundtrip/time-signatures-3-4-time.ly", "utf-8");
-const doc = decode(ly);
-console.log(JSON.stringify(doc, null, 2));
